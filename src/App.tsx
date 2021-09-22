69812bdd7a595f81c8ad487853b97621ec5cbf2a
Franwernke <franwernke@usp.br>
2021-09-22 19:48:43 -0300

    Clean useless template generated files
    Signed-off-by: Franwernke <franwernke@usp.br>
    Co-authored-by: Ian Galvao <ian.galvao@usp.br>
    Co-authored-by: Vinicius Frota <viniciuspxfrota@gmail.com>
    Co-authored-by: João Daniel <jotaf.daniel@gmail.com>
    Co-authored-by: Erick Rodrigues <erick.rodrigues.santana14@gmail.com>
    Co-authored-by: Lucas Fujiwara <lucastfujiwara@gmail.com>
    Co-authored-by: Thiago Guerrero <tgbalera@hotmail.com>
    Co-authored-by: Daniel Leal <os.leal90@gmail.com>
    
    Squashed commit of the following:
    
    commit 0f44d5b3a8c6001b80b1d944f1bf35d343197c13
    Author: Franwernke <franwernke@usp.br>
    Date:   Wed Sep 22 19:48:18 2021 -0300
    
        mob next
    
    Signed-off-by: Franwernke <franwernke@usp.br>

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,9 @@
-import React, { useState } from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import React from 'react';
 
-function Vinicius(props: { message: string; outraCoisa: string }) {
-  const [state, setState] = useState(0);
-
-  return (
-    <>
-      <div>
-        <button onClick={() => setState(state + 1)}>+</button>
-      </div>
-
-      <div> {state}</div>
-    </>
-  );
-}
-
-function App() {
+function App(): JSX.Element {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <Vinicius message="teste" outraCoisa="testando" />
-      </header>
+      <h1>Bananas de Pijama</h1>
     </div>
   );
 }

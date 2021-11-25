60fa21e989d2d780fc15612496371aad3085fd5f
Lucas Toshio Löschner Fujiwara <40678033+lfujiwara@users.noreply.github.com>
2021-11-25 17:29:40 -0300

    Update routes.tsx

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 import PreRegistration from './pages/PreRegistration/PreRegistration';
 import Search from './pages/Search';
 import Home from './pages/Home';

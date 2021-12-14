b180c536e48b8b05132cbff41c60e75cf43df122
Thiago Guerrero <tgbalera@hotmail.com>
2021-12-14 19:14:36 -0300

    remove hardcoded
    Signed-off-by: Thiago Guerrero <tgbalera@hotmail.com>

--- src/hooks/Profile.ts
+++ src/hooks/Profile.ts
@@ -3,11 +3,5 @@ import { AuthContext } from '../contexts/auth.context';
 
 export const useProfile = () => {
   const [state] = useContext(AuthContext);
-  return {
-    _id: 'handmade',
-    name: 'Thiago Guerrero',
-    email: 'tguerrero@usp.br',
-    roles: ['Usuário', 'Gestão CEIP'],
-  };
   return state.profile;
 };

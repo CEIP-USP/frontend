0d8825186ba0355a291b66e289a2453e8d8b2456
Lucas Toshio Löschner Fujiwara <lucastfujiwara@gmail.com>
2022-01-18 19:04:44 -0300

    feature: add get/edit methods to profile.service.ts

--- src/services/profile/profile.service.ts
+++ src/services/profile/profile.service.ts
@@ -1,9 +1,43 @@
 import { AxiosInstance } from 'axios';
+import { Document } from '../../components/Forms/Forms.interface';
+
+export type EditProfilePayload = {
+  name?: string;
+  email?: string;
+  document?: Document;
+  phone?: string;
+  address?: string;
+  dayOfSecondShot?: Date;
+};
 
 export class ProfileService {
   constructor(private _axios: AxiosInstance) {}
 
+  public getProfile(id: string) {
+    return this._axios
+      .get<{
+        _id: string;
+        name: string;
+        email: string;
+        document?: {
+          type: string;
+          value?: string;
+        };
+        phone: string;
+        address: string;
+        dayOfSecondShot: string;
+      }>(`/profiles/${id}`)
+      .then(({ data }) => ({
+        ...data,
+        dayOfSecondShot: new Date(data.dayOfSecondShot),
+      }));
+  }
+
   public deleteProfile(id: string) {
     return this._axios.delete(`/profiles/${id}`);
+  }
+
+  public editProfile(id: string, payload: EditProfilePayload) {
+    return this._axios.put(`/profiles/${id}`, payload);
   }
 }
